513d7ff3f314f4df02e73f93c36aea8b1a7674a7
thealexgroup <30634252+thealexgroup@users.noreply.github.com>
2018-01-08 13:28:14 -0600

    mostly directory structure, not much work yet

---
+++ server.js
@@ -0,0 +1,7 @@
+//require express
+var express = require("express");
+//require method-override
+var methodOverride = require("method-override");
+//require body-parser
+var bodyParser = require("body-parser");
+
